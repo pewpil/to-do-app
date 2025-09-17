201ec4a5b1cba1a34fbccf0bbc4a46e9ff4dcbe3
pewpil <pewpil@proton.me>
2025-09-17 12:13:47 +0800

    Task Categories page finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import SideBar from "./SideBar/SideBar";
 import Dashboard from "./Dashboard/Dashboard";
 import MyTask from "./MyTask/MyTask";
 import VitalTask from "./VitalTask/VitalTask";
+import TaskCategories from "./TaskCategories/TaskCategories";
 
 function App() {
   return (
     <div id={styles.app}>
 	  <TopBar title="Dashboard" day="Tuesday" date="20 September"></TopBar>
 	  <SideBar></SideBar>
-	  <VitalTask></VitalTask>
+	  <TaskCategories></TaskCategories>
     </div>
   );
 }
---
+++ src/TaskCategories/TaskCategories.jsx
@@ -0,0 +1,45 @@
+"use strict";
+import styles from "./TaskCategories.module.css";
+import Section from "../Section/Section";
+import Table from "./Table/Table";
+
+function TaskCategories() {
+	return (
+		<div id={styles.task_categories}>
+			<Section
+				title="Task Categories"
+				action={
+					<button>
+						<span className="material-symbols-outlined">add</span>Add Category
+					</button>
+				}
+				content={
+					<>
+						<Section
+							title="Task Status"
+							action={
+								<button>
+									<span className="material-symbols-outlined">add</span>Add Task
+									Status
+								</button>
+							}
+							content={<Table></Table>}
+						></Section>
+						<Section
+							title="Task Priority"
+							action={
+								<button>
+									<span className="material-symbols-outlined">add</span>Add Task
+									Priority
+								</button>
+							}
+					content={<Table></Table>}
+						></Section>
+					</>
+				}
+			></Section>
+		</div>
+	);
+}
+
+export default TaskCategories;
