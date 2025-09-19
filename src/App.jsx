d8d9cd884b6ce7db7acadd541433f44cfc2f37cb
pewpil <pewpil@proton.me>
2025-09-19 10:15:04 +0800

    Account page finished

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ import MyTask from "./MyTask/MyTask";
 import VitalTask from "./VitalTask/VitalTask";
 import TaskCategories from "./TaskCategories/TaskCategories";
 import Login from "./Login/Login";
+import Account from "./Account/Account";
 
 function App() {
   return (
     <div id={styles.app}>
-	  <Login></Login>
+	  <TopBar title="Dashboard" day="Tuesday" date="20 June"></TopBar>
+	  <SideBar></SideBar>
+	  <Account></Account>
     </div>
   );
 }
