23a508b0fffc0f3f151ed2b8732df46d14df7e3c
pewpil <pewpil@proton.me>
2025-09-24 20:56:49 +0800

    tasks in dashboard needs to be worked on for the design

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -6,57 +6,58 @@ import styles from "./Dashboard.module.css";
 import universal from "../Universal.module.css";
 
 function Dashboard() {
-	const els = [<Task></Task>, <Task></Task>];
-	return (
-		<div id={styles.dashboard}>
-			<div id={styles.welcome_and_colab}>
-				<h1 className={universal.title1}>Welcome back, Sundar!</h1>
-				{/* Image of colaborator's pfp's here */}
-				{/* invite button here */}
-			</div>
-			<div id={styles.board}>
-				<Section
-					title={
-						<>
-							<span class="material-symbols-outlined">pending_actions</span>
-							To-do
-						</>
-					}
-					action={
-						<>
-							<span class="material-symbols-outlined">add</span>
-							Add task
-						</>
-					}
-					date="20 June"
-					day="Today"
-					content={els}
-				></Section>
-				<div id={styles.stat_and_completed}>
-					<Section
-						title={
-							<>
-								<span class="material-symbols-outlined">inventory</span>
-								Task Status
-							</>
-						}
-						content={<Status></Status>}
-					></Section>
-					<Section
-						title={
-							<>
-								<span class="material-symbols-outlined">
-									assignment_turned_in
-								</span>
-								Completed Tasks
-							</>
-						}
-						content={els}
-					></Section>
-				</div>
-			</div>
-		</div>
-	);
+  const els = [<Task></Task>, <Task></Task>];
+  return (
+    <div id={styles.dashboard}>
+      <div id={styles.welcome_and_colab}>
+        <h1 className={universal.title1}>Welcome back, Sundar!</h1>
+        {/* Image of colaborator's pfp's here */}
+        {/* invite button here */}
+      </div>
+      <div id={styles.board}>
+        <Section
+          page_style="dashboard_todo"
+          title={
+            <>
+              <span class="material-symbols-outlined">pending_actions</span>
+              To-do
+            </>
+          }
+          action={
+            <>
+              <span class="material-symbols-outlined">add</span>
+              Add task
+            </>
+          }
+          date="20 June"
+          day="Today"
+          content={els}
+        ></Section>
+        <div id={styles.stat_and_completed}>
+          <Section
+            title={
+              <>
+                <span class="material-symbols-outlined">inventory</span>
+                Task Status
+              </>
+            }
+            content={<Status></Status>}
+          ></Section>
+          <Section
+            title={
+              <>
+                <span class="material-symbols-outlined">
+                  assignment_turned_in
+                </span>
+                Completed Tasks
+              </>
+            }
+            content={els}
+          ></Section>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
