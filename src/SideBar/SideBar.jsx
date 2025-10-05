96b47d60aa625551f0e6c9cb3bccef9948f66c25
pewpil <pewpil@proton.me>
2025-10-05 14:38:46 +0800

    icons resized (made them bigger

--- src/SideBar/SideBar.jsx
+++ src/SideBar/SideBar.jsx
@@ -3,65 +3,70 @@ import styles from "./SideBar.module.css";
 import universal from "../Universal.module.css";
 
 function SideBar() {
-  return (
-    <div id={styles.sidebar}>
-      <div id={styles.profile}>
-        <div id={styles.profile_container}>
-          <img
-            src="images/pfp_girl.webp"
-            alt="pfp girl"
-            id={styles.profile_pic}
-            className={universal.img_placeholder}
-          />
-          <h2 className={universal.title3} id={styles.name}>
-            Sundar Gurung
-          </h2>
-          <h3 className={universal.title3} id={styles.email}>
-            sundargurung360@gamil.com
-          </h3>
-        </div>
-      </div>
-      <div id={styles.choices}>
-        <div className={styles.choice_container}>
-          <span className="material-symbols-outlined fs3rem">dashboard</span>
-          <h3 className={`${universal.title3} ${styles.choice}`}>Dashboard</h3>
-        </div>
-        <div className={styles.choice_container}>
-          <span className="material-symbols-outlined fs3rem">
-            priority_high
-          </span>
-          <h3 className={`${universal.title3} ${styles.choice}`}>Vital Task</h3>
-        </div>
-        <div className={styles.choice_container}>
-          <span className="material-symbols-outlined fs3rem">
-            assignment_turned_in
-          </span>
-          <h3 className={`${universal.title3} ${styles.choice}`}>My Task</h3>
-        </div>
-        <div className={styles.choice_container}>
-          <span className="material-symbols-outlined fs3rem">list_alt</span>
-          <h3 className={`${universal.title3} ${styles.choice}`}>
-            Task Categories
-          </h3>
-        </div>
-        <div className={styles.choice_container}>
-          <span className="material-icons fs3rem">settings</span>
-          <h3 className={`${universal.title3} ${styles.choice}`}>Settings</h3>
-        </div>
-        <div className={styles.choice_container}>
-          <span className="material-icons fs3rem">help</span>
-          <h3 className={`${universal.title3} ${styles.choice}`}>Help</h3>
-        </div>
-      </div>
-      <div id={styles.logout}>
-        <div className={styles.choice_container}></div>
-        <h3 className={`${universal.title3} ${styles.choice}`}>
-          <span className="material-symbols-outlined fs3rem">logout</span>
-          Logout
-        </h3>
-      </div>
-    </div>
-  );
+	return (
+		<div id={styles.sidebar}>
+			<div id={styles.profile}>
+				<div id={styles.profile_container}>
+					<img
+						src="images/pfp_girl.webp"
+						alt="pfp girl"
+						id={styles.profile_pic}
+						className={universal.img_placeholder}
+					/>
+					<h2 className={universal.title3} id={styles.name}>
+						Sundar Gurung
+					</h2>
+					<h3 className={universal.title3} id={styles.email}>
+						sundargurung360@gamil.com
+					</h3>
+				</div>
+			</div>
+			<div id={styles.choices}>
+				<div className={styles.choice_container}>
+					<span className={`${"material-symbols-outlined"} ${styles.icon}`}>
+						dashboard
+					</span>
+					<h3 className={`${universal.title3} ${styles.choice}`}>Dashboard</h3>
+				</div>
+				<div className={styles.choice_container}>
+					<span className={`${"material-symbols-outlined"} ${styles.icon}`}>
+						priority_high
+					</span>
+					<h3 className={`${universal.title3} ${styles.choice}`}>Vital Task</h3>
+				</div>
+				<div className={styles.choice_container}>
+					<span className={`${"material-symbols-outlined"} ${styles.icon}`}>
+						assignment_turned_in
+					</span>
+					<h3 className={`${universal.title3} ${styles.choice}`}>My Task</h3>
+				</div>
+				<div className={styles.choice_container}>
+					<span className={`${"material-symbols-outlined"} ${styles.icon}`}>
+						list_alt
+					</span>
+					<h3 className={`${universal.title3} ${styles.choice}`}>
+						Task Categories
+					</h3>
+				</div>
+				<div className={styles.choice_container}>
+					<span className={`${"material-icons"} ${styles.icon}`}>settings</span>
+					<h3 className={`${universal.title3} ${styles.choice}`}>Settings</h3>
+				</div>
+				<div className={styles.choice_container}>
+					<span className={`${"material-icons"} ${styles.icon}`}>help</span>
+					<h3 className={`${universal.title3} ${styles.choice}`}>Help</h3>
+				</div>
+			</div>
+			<div id={styles.logout}>
+				<div className={styles.choice_container}>
+					<span className={`${"material-symbols-outlined"} ${styles.icon}`}>
+						logout
+					</span>
+					<h3 className={`${universal.title3} ${styles.choice}`}>Logout</h3>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default SideBar;
