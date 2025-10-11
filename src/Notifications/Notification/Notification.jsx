14cce87e15670d3c6bdf75ecc5039dabec56579e
pewpil <pewpil@proton.me>
2025-10-11 21:14:08 +0800

    made a notification sample

--- src/Notifications/Notification/Notification.jsx
+++ src/Notifications/Notification/Notification.jsx
@@ -1,11 +1,24 @@
 "use strict";
 import styles from "./Notification.module.css";
+import universal from "../../Universal.module.css";
 
 function Notification() {
-	return (
-		<div id={styles.notification}>
-		</div>
-	);
+  return (
+    <div id={styles.notification}>
+      <div id={styles.meta}>
+        <p className={`${universal.body1} ${styles.task_info}`}>
+          Complete the UI design of Landing Page for FoodVentures.
+          <span id={styles.time}>2h</span>
+        </p>
+        <p className={`${universal.body1} ${styles.task_prio}`}>
+          Priority: <span id={styles.priority}>High</span>
+        </p>
+      </div>
+      <div id={styles.img}>
+        <img src="images/task_img.webp" id={styles.task_img} />
+      </div>
+    </div>
+  );
 }
 
 export default Notification;
--- src/Notifications/Notifications.jsx
+++ src/Notifications/Notifications.jsx
@@ -24,6 +24,7 @@ function Notifications() {
         </div>
       </div>
       <div>
+        <Notification></Notification>
       </div>
     </div>
   );
