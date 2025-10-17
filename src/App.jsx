5165ac70c147d792c54ba5c2e549f42b820749cf
pewpil <pewpil@proton.me>
2025-10-17 18:04:24 +0800

    made notifications be able to toggle

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Account from "./Account/Account";
 import Signup from "./Signup/Signup";
 import Notifications from "./Notifications/Notifications";
 import AddTask from "./AddTask/AddTask";
+import { useState } from "react";
 
 // function App() {
 // 	return (
@@ -20,26 +21,30 @@ import AddTask from "./AddTask/AddTask";
 // }
 
 function App() {
-  return (
-    <div id={styles.app}>
-      <TopBar title="Dashboard" date="10/12/2002" day="Monday"></TopBar>
-      <AddTask></AddTask>
-      <BrowserRouter>
-        <SideBar></SideBar>
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
-          <Route path="/vital-task" element={<VitalTask></VitalTask>}></Route>
-          <Route path="/my-task" element={<MyTask></MyTask>}></Route>
-          <Route
-            path="/task-categories"
-            element={<TaskCategories></TaskCategories>}
-          ></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <Route path="/account" element={<Account></Account>}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	const [displayNotification, setDisplayNotification] = useState(false);
+	function toggleDisplayNotification() {
+		setDisplayNotification(!displayNotification);
+	}
+	return (
+		<div id={styles.app}>
+			<TopBar title="Dashboard" date="10/12/2002" day="Monday" notification_click={toggleDisplayNotification}></TopBar>
+			{displayNotification ? <Notifications></Notifications> : <></>}
+			<BrowserRouter>
+				<SideBar></SideBar>
+				<Routes>
+					<Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+					<Route path="/vital-task" element={<VitalTask></VitalTask>}></Route>
+					<Route path="/my-task" element={<MyTask></MyTask>}></Route>
+					<Route
+						path="/task-categories"
+						element={<TaskCategories></TaskCategories>}
+					></Route>
+					<Route path="/login" element={<Login></Login>}></Route>
+					<Route path="/account" element={<Account></Account>}></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
